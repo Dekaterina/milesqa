37d107f5bccd5aa43aaeab95b98e9d984d761678
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-10-30 10:46:53 +0300

    gitignore changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-        int result;
         int mile = price / 20;
-        result = mile;
-        return result;
+        return mile;
     }
 }
 
